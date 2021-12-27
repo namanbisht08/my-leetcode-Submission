80176b0633a762ae1104f2c8e33c6b99eff5fba1
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2021-12-27 17:55:17 +0530

    Update find-peak-element.cpp

--- find-peak-element/find-peak-element.cpp
+++ find-peak-element/find-peak-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        vector<int> v(nums);
-        sort(v.begin(), v.end());
+        vector<int> v(nums);        //make copy of nums
+        sort(v.begin(), v.end());  //sort vector v in assesending order
         
         int n=0;
         for(int it : nums)
         {
-            if(it == *(v.end()-1))
+            if(it == *(v.end()-1))  //if the (value it) == (the largest value present in vector) return count of itreation.
                 return n;
             n++;
         }
