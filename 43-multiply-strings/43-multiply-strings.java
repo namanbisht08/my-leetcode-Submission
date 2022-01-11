441282e82d2a7f5fbc62e2cab9f8826a976a184d
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-01-11 13:48:56 +0530

    Time: 3 ms (93.41%), Space: 38.8 MB (91.13%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -18,9 +18,9 @@ class Solution {
         int len2 = num2.length();
         int len = len1 + len2;
         int[] product = new int[len];
-        for (int i = num1.length() - 1; i >= 0; i--) {
+        for (int i = len1 - 1; i >= 0; i--) {
             int a = num1.charAt(i) - '0';
-            for (int j = num2.length() - 1; j >= 0; j--) {
+            for (int j = len2 - 1; j >= 0; j--) {
                 int b = num2.charAt(j) - '0';
                 int idx = i + j + 1;
                 int p = a * b + product[idx];
@@ -30,7 +30,7 @@ class Solution {
         }
         
         int start = product[0] != 0 ? 0 : 1;
-        StringBuilder result = new StringBuilder();
+        StringBuffer result = new StringBuffer();
         for (int i = start; i < len; i++) {
             result.append(product[i]);
         }
