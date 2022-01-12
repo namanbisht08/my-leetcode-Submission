d433c398f5da29d615d5b5da6b59ec0a41097ec8
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-01-12 17:32:04 +0530

    Time: 12 ms (98.83%), Space: 13.6 MB (51.53%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int('0b'+a,2)+int('0b'+b,2))[2:]
