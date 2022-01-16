31df8effc2b94790e7bc5086827c4e67c373b22e
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-01-16 22:15:04 +0530

    Time: 122 ms (9.29%), Space: 13.5 MB (13.70%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if ''.join(reversed(str(x)))==str(x): return True
-        return False
-        
-        
+        return True if str(x)==str(x)[::-1] else False
         
