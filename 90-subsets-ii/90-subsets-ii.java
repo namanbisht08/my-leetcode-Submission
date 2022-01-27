d8959621fe59f98970fef9f6704c9e9dd6c14ef9
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-01-27 10:43:01 +0530

    Time: 6 ms (13.35%), Space: 43.7 MB (6.87%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,9 +1,11 @@
+//Brute force approach
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] arr) {
         Arrays.sort(arr);
-        Set<List<Integer>> s = new HashSet<>();
+        Set<List<Integer>> s = new HashSet<>();  //taking set so that no duplicate List get appended.
         fun(0, arr, s, new ArrayList<Integer>());
-        List<List<Integer>> l = new ArrayList<>(s);
+        List<List<Integer>> l = new ArrayList<>(s); //converting the set back to list [list is the req. return type.]
         return l;
     }
     
@@ -11,15 +13,15 @@ class Solution {
     {
         if(i==arr.length)
         {
-            Collections.sort(ds);
+            Collections.sort(ds);      //sorting before appending, for making every list symmertic to compare
             s.add(new ArrayList(ds));
             return;
         }
         
-        ds.add(arr[i]);
+        ds.add(arr[i]);           //element pick 
         fun(i+1, arr, s, ds);
         
-        ds.remove(ds.size()-1);
+        ds.remove(ds.size()-1);  //element not pick
         fun(i+1, arr, s, ds);
     }
 }
