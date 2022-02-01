2063375fb8d1d2e911a28b617bd2a5a88a71caa1
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-01 10:05:58 +0530

    Time: 1 ms (32.34%), Space: 42.6 MB (5.06%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,25 +13,45 @@
  *     }
  * }
  */
+
+//iterative solution :-
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         Stack<TreeNode> s = new Stack<>();
+//         List<Integer> l = new ArrayList<>();
+//         if(root==null)
+//             return l;
+//         s.push(root);
+//         while(!s.empty())
+//         {
+//             TreeNode node =s.pop();
+//             if(node!=null)
+//             {
+//                 l.add(node.val);
+//                 if(node.right!=null) s.push(node.right);
+//                 if(node.left!=null) s.push(node.left);
+//             }
+//             else
+//                 continue;
+//         }
+//     return l;
+//     }
+// }
+
+
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> s = new Stack<>();
-        List<Integer> l = new ArrayList<>();
+     public List<Integer> preorderTraversal(TreeNode root) {
+         List<Integer> l = new ArrayList<Integer>();
+         helper(root,l);
+         return l;
+     }
+    
+    void helper(TreeNode root, List<Integer> l)
+    {
         if(root==null)
-            return l;
-        s.push(root);
-        while(!s.empty())
-        {
-            TreeNode node =s.pop();
-            if(node!=null)
-            {
-                l.add(node.val);
-                if(node.right!=null) s.push(node.right);
-                if(node.left!=null) s.push(node.left);
-            }
-            else
-                continue;
-        }
-    return l;
+            return;
+        l.add(root.val);
+        helper(root.left,l);
+        helper(root.right,l);
     }
 }
