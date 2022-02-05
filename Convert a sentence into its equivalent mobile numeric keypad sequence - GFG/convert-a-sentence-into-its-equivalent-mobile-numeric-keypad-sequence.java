22736a018b940ab9f9feaaa835b855eb56321da1
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-05 20:05:35 +0530

    Added solution - LeetHub

---
+++ Convert a sentence into its equivalent mobile numeric keypad sequence - GFG/convert-a-sentence-into-its-equivalent-mobile-numeric-keypad-sequence.java
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            String S = read.readLine();
+            Solution obj = new Solution();
+            System.out.println(obj.printSequence(S));
+            
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    String printSequence(String s) 
+    { 
+        StringBuffer sb =new StringBuffer();
+        String hash[]={"2", "22", "222", "3", "33", "333", "4", "44", "444", 
+                         "5", "55", "555", "6", "66", "666", "7", "77", "777",
+                         "7777","8", "88", "888", "9", "99", "999", "9999"};
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)==' ')
+                sb.append("0");
+            else
+                sb.append(hash[s.charAt(i)-65]);
+        }
+         
+        return sb.toString();                
+    }
+}
