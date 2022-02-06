57e3e347e92b4474ea30f269a6567dfbf83cf1c0
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-07 00:10:03 +0530

    Time: 2427 ms (5.06%), Space: 622.3 MB (5.09%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,65 +1,49 @@
-//brute force : 
-// class Solution {
-//     public List<List<String>> groupAnagrams(String[] strs) {
-//         int map[]=new int[strs.length];
-//         List<List<String>> l = new ArrayList<>();
-//         int n=strs.length;
-//         for(int i=0; i<n; i++)
-//         {
-//             List<String> sl = new ArrayList<>();
-//             if(map[i]!=0)
-//                 continue;
-//             sl.add(strs[i]);
-//             map[i]++;
-//             for(int j=i+1; j<n; j++)
-//             { 
-//                 if(isAnagram(strs[i],strs[j]))
-//                 {
-//                     sl.add(strs[j]);
-//                     map[j]++;
-//                 }
-//             }
-//             l.add(new ArrayList<>(sl));
-//         }
-//         return l;
-//     }
-    
-//     boolean isAnagram(String str1, String str2)
-//         {
-//             int map[]=new int[26];
-//             if(str1.length()!=str2.length())
-//                 return false;
-//             char[] ch1=str1.toCharArray();
-//             char[] ch2=str2.toCharArray();
-//             int n=str1.length();
-            
-//             for(int i=0; i<n; i++)
-//             {
-//                 map[ch1[i]-97]++;
-//                 map[ch2[i]-97]--;
-//             }
-            
-//             for(int i=0; i<26; i++)
-//             {
-//                 if(map[i]!=0)
-//                     return false;
-//             }
-            
-//             return true;
-//         }
-// }
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) return new ArrayList();
-        Map<String, List> ans = new HashMap<String, List>();
-        for (String s : strs) {
-            char[] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String key = String.valueOf(ca);
-            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
-            ans.get(key).add(s);
+        int map[]=new int[strs.length];
+        List<List<String>> l = new ArrayList<>();
+        int n=strs.length;
+        for(int i=0; i<n; i++)
+        {
+            List<String> sl = new ArrayList<>();
+            if(map[i]!=0)
+                continue;
+            sl.add(strs[i]);
+            map[i]++;
+            for(int j=i+1; j<n; j++)
+            { 
+                if(isAnagram(strs[i],strs[j]))
+                {
+                    sl.add(strs[j]);
+                    map[j]++;
+                }
+            }
+            l.add(new ArrayList<>(sl));
         }
-        return new ArrayList(ans.values());
+        return l;
     }
+    
+    boolean isAnagram(String str1, String str2)
+        {
+            int map[]=new int[26];
+            if(str1.length()!=str2.length())
+                return false;
+            char[] ch1=str1.toCharArray();
+            char[] ch2=str2.toCharArray();
+            int n=str1.length();
+            
+            for(int i=0; i<n; i++)
+            {
+                map[ch1[i]-97]++;
+                map[ch2[i]-97]--;
+            }
+            
+            for(int i=0; i<26; i++)
+            {
+                if(map[i]!=0)
+                    return false;
+            }
+            
+            return true;
+        }
 }
