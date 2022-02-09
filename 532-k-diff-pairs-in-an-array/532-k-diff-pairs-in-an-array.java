d7743d3dbc061a9a6b9fd711a8fe1ef2cecf1b1a
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-09 11:55:35 +0530

    Time: 14 ms (46.69%), Space: 48.6 MB (9.44%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,26 +1,26 @@
-public class Solution {
+class Solution {
     public int findPairs(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k < 0)   return 0;
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        if (k < 0) {
+            return 0;
         }
-        
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (k == 0) {
-                //count how many elements in the array that appear more than twice.
-                if (entry.getValue() >= 2) {
-                    count++;
-                } 
-            } else {
-                if (map.containsKey(entry.getKey() + k)) {
-                    count++;
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                if (k == 0 && map.get(i) == 1) {
+                    result++;
                 }
+                map.put(i, map.get(i) + 1);
+            } else {
+                if (map.containsKey(i - k)) {
+                    result++;
+                }
+                if (map.containsKey(i + k)) {
+                    result++;
+                }
+                map.put(i, 1);
             }
         }
-        
-        return count;
+        return result;
     }
 }
