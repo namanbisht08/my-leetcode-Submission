c8b31d1f526f8c5d087127dab8a81ccf00e5ff1f
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-13 10:59:32 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (23.91%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,23 +1,23 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] arr) {
-        List<List<Integer>> l = new ArrayList<>();
-        fun(0,arr,l,new ArrayList<Integer>());
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> l = new ArrayList();
+        List<Integer> sl = new ArrayList();
+        helper(0,nums, l, sl);
         return l;
     }
-    
-    void fun(int i, int[] arr, List<List<Integer>> l, List<Integer> ds)
+  
+  void helper(int ind, int[] nums, List<List<Integer>> l, List<Integer> sl)
+  {
+    if(ind==nums.length)
     {
-        if(i==arr.length)
-        {
-            l.add(new ArrayList<Integer>(ds));
-            return;
-        }
-        
-        ds.add(arr[i]);
-        fun(i+1,arr,l,ds);  //pick
-        
-        ds.remove(ds.size()-1);
-        fun(i+1,arr,l,ds);  //not pick
+      l.add(new ArrayList<>(sl));
+      return;
     }
     
+    sl.add(nums[ind]);
+    helper(ind+1,nums,l,sl);
+    
+    sl.remove(sl.size()-1);
+    helper(ind+1,nums,l,sl);
+  }
 }
