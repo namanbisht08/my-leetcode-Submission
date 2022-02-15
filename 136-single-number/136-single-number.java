00646b785b40724923f1ceea6ec2b7fdaf7eab62
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-15 08:19:52 +0530

    Time: 1 ms (95.92%), Space: 50.2 MB (34.10%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,17 +1,28 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-      Map<Integer,Integer> map = new HashMap();
-      for(int i: nums)
-      {
-        int k=map.getOrDefault(i,0);
-        map.put(i,k+1);
-      }
+// //Brute force :
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//       Map<Integer,Integer> map = new HashMap();
+//       for(int i: nums)
+//       {
+//         int k=map.getOrDefault(i,0);
+//         map.put(i,k+1);
+//       }
       
-      for (Map.Entry<Integer, Integer> me : map.entrySet())
-      {
-        if(me.getValue()==1)
-          return me.getKey();
-      }
-      return 1;
-    }
+//       for (Map.Entry<Integer, Integer> me : map.entrySet())
+//       {
+//         if(me.getValue()==1)
+//           return me.getKey();
+//       }
+//       return 1;
+//     }
+// }
+
+// same XOR same=0
+class Solution{
+  public int singleNumber(int[] nums){
+    int ans=0;
+    for(int i=0; i<nums.length; i++)
+      ans^=nums[i];
+    return ans;
+  }
 }
