334d6f85a201008c1e4c6433d35be3ba22ea0c53
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-17 19:50:33 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (39.12%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,23 +1,48 @@
+
+
+
 class Solution {
+
     public List<List<Integer>> combinationSum(int[] arr, int t) {
+
         List<List<Integer>> r = new ArrayList<>();
+
         fun(0,arr,t,r,new ArrayList<Integer>());
+
         return r;
+
     }
+
     
+
     void fun(int ind, int arr[], int t, List<List<Integer>> r, List<Integer> ds)
+
     {
+
         if(ind==arr.length){
+
             if(t==0)
+
                 r.add(new ArrayList<>(ds));
+
             return;
+
         }
+
         
+
         if(arr[ind] <= t){
+
             ds.add(arr[ind]);
+
             fun(ind, arr, t-arr[ind], r, ds);
+
             ds.remove(ds.size()-1);
+
         }
+
         fun(ind+1, arr, t, r, ds);  
+
     }
+
 }
