a36e22a0e70674b1df874a747c7fb10290761e06
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-20 10:45:58 +0530

    Time: 139 ms (46.75%), Space: 14.4 MB (96.36%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -1,3 +1,8 @@
+# Sort intervals in such an order that only previous ones are possible to cover current one.
+
+# Sort by the left bound, and when left bounds are equal, sort right bounds by reverse order; Therefore, no interval can cover previous ones;
+# Loop through the intervals, whenever current right most bound < next interval's right bound, it means current interval can NOT cover next interval, update right most bound and increase counter by 1.
+
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
       count=cur=0
