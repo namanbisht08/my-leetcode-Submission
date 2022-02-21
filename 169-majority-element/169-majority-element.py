01171fea15ea7f2756f2906cb94ffad68b2cda3a
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-02-21 20:07:52 +0530

    Time: 160 ms (95.59%), Space: 15.5 MB (43.74%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,4 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-      return sorted(list(collections.Counter(nums).items()), key=lambda n: n[1], reverse=True)[0][0]
+      return sorted(nums)[len(nums)//2]
+      #return sorted(list(collections.Counter(nums).items()), key=lambda n: n[1], reverse=True)[0][0]
         
