f406b6996f72c6eb639a1c56b701e9805198a2f4
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-10-13 13:06:42 +0530

    Time: 5 ms (44.93%), Space: 48.6 MB (12.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,25 @@
 class Solution {
-    int helper(int n)
+    int helper(int n, int[] dp)
     {
       if(n==1) return 1;
       if(n==0) return 0;
+      if(dp[n]!=-1) return dp[n];
       
-      if(n%2==0) return helper(n/2);
-      return helper(n/2)+1;
+      if(n%2==0){
+        dp[n]=helper(n/2, dp);
+        return dp[n];
+      }
+      dp[n]=helper(n/2, dp)+1;
+      return dp[n];
     }
     
     public int[] countBits(int n) {
+      int dp[] = new int[n+1];
       int ans[] = new int[n+1];
+      Arrays.fill(dp,-1);
       
       for(int i=0; i<=n; i++)
-        ans[i]=helper(i);
+        ans[i]=helper(i, dp);
         
       return ans;
     }
