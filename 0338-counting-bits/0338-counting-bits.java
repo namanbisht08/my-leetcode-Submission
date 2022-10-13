e8db058359693e5e6f95dfde249363619b8af4b4
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2022-10-13 13:07:36 +0530

    Time: 5 ms (44.93%), Space: 47.8 MB (71.03%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,7 +3,7 @@ class Solution {
     {
       if(n==1) return 1;
       if(n==0) return 0;
-      if(dp[n]!=-1) return dp[n];
+      if(dp[n]!=0) return dp[n];
       
       if(n%2==0){
         dp[n]=helper(n/2, dp);
@@ -16,7 +16,7 @@ class Solution {
     public int[] countBits(int n) {
       int dp[] = new int[n+1];
       int ans[] = new int[n+1];
-      Arrays.fill(dp,-1);
+      //Arrays.fill(dp,-1);
       
       for(int i=0; i<=n; i++)
         ans[i]=helper(i, dp);
